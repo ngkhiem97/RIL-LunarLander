b2169bae3cb0339eec690d9b962d90d04848f4f8
Khiem Nguyen <ng.khiem97vn@gmail.com>
2022-06-27 01:38:23 -0400

    update

--- src/env.py
+++ src/env.py
@@ -1,18 +1,14 @@
 import gym
-
-env = gym.make("ALE/SpaceInvaders-v5", render_mode="human")
+env = gym.make("LunarLander-v2")
 env.action_space.seed(42)
 
-observation, info = env.reset(seed=42, return_info=True)
+env.reset()
 
-episodes = 10
-for i in range(episodes):
+for _ in range(1000):
+    observation, reward, done, info = env.step(env.action_space.sample())
     env.render()
-    action = env.action_space.sample()
-    observation, reward, done, info = env.step(action)
+
     if done:
-        observation, info = env.reset(return_info=True)
-        print("Episode finished after {} timesteps".format(_))
-        break
+        env.reset()
 
 env.close()
